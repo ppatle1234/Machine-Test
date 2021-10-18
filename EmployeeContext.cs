b00c4f474b8a783d9bdc81d981ab859171fbb1b2
Prashant Patle <90300188+ppatle1234@users.noreply.github.com>
2021-10-18 16:07:47 +0530

    Add files via upload

---
+++ EmployeeContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace MachineTest.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext():base("Constr")
+        {
+          
+
+        }
+        public DbSet<EmployeeModel> EmployeeModels { get; set; }
+
+
+    }
+}
---
+++ EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MachineTest.Models
+{
+    public class EmployeeModel
+    {
+        [Key]//assign primary key and identify column
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
